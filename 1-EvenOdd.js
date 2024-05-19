3158ad96dab64b68f3c41fde0343a020b717b5e7
Dystian En <dystian.en@kelaspintar.id>
2024-05-19 19:14:00 +0700

    Day 1 - Even Odd

--- 1-EvenOdd.js
+++ 1-EvenOdd.js
@@ -1,8 +1,8 @@
 // DAY : 1
 // URL : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
 // assert.strictEqual(evenOrOdd(-42), "Even");
-// assert.strictEqual(evenOrOdd(-43), "Odd");
+// assert.strictEqual(evenOrOdd(-43), "Odd"); 
 
 function evenOrOdd(number) {
-  return number % 2 === 0 ? "Even" : "Odd"
-}
+  return number % 2 === 0 ? "Even" : "Odd";
+};
